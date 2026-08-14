dfb51e9b73a5a7382dae2c7d068b006a6a40bb3b
rgremaud <rgremau@gmail.com>
2026-08-13 22:00:47 -0400

    Work on API call (currently broken)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
 import { useState, useEffect } from "react";
-import { data } from "./data";
+import { shuffle } from "./data";
 // import reactLogo from './assets/react.svg' - keep to view format for importing assets
 
+function Image({ pokemon }) {
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((response) => response.json())
+      .then((json) => setInfo(json))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const imageUrl = info.sprites.front_default;
+
+  return imageUrl;
+}
+
 function App() {
-  /* 
-  state variables to track:
-  score - track the current run 
-  high - track the high score
-  */
   const [score, setScore] = useState(0);
   const [high, setHigh] = useState(0);
   const [values, setValues] = useState([]);
+  const data = [
+    { id: 1, image: Image("pikachu") },
+    { id: 2, image: "" },
+    { id: 3, image: "" },
+    { id: 4, image: "" },
+    { id: 5, image: "" },
+    { id: 6, image: "" },
+    { id: 7, image: "" },
+    { id: 8, image: "" },
+    { id: 9, image: "" },
+    { id: 10, image: "" },
+  ];
 
   useEffect(() => {
     if (high < score) {
@@ -19,8 +41,14 @@ function App() {
   }, [score]);
 
   function handleClick(id) {
-    setScore((prevCount) => prevCount + 1);
-    setValues([...values, id]);
+    if (!values.includes(id)) {
+      setScore((prevCount) => prevCount + 1);
+      setValues([...values, id]);
+    } else {
+      setScore(0);
+      setValues([]);
+    }
+    shuffle(data);
   }
 
   return (
@@ -36,6 +64,7 @@ function App() {
             onClick={() => handleClick(box.id)}
           >
             {box.id}
+            {box.image}
           </button>
         ))}
       </div>
--- src/data.js
+++ src/data.js
@@ -1,17 +1,19 @@
+import { useState, useEffect } from "react";
+
 export const data = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 },
-  { id: 4 },
-  { id: 5 },
-  { id: 6 },
-  { id: 7 },
-  { id: 8 },
-  { id: 9 },
-  { id: 10 },
+  { id: 1, image: Image("pikachu") },
+  { id: 2, image: "" },
+  { id: 3, image: "" },
+  { id: 4, image: "" },
+  { id: 5, image: "" },
+  { id: 6, image: "" },
+  { id: 7, image: "" },
+  { id: 8, image: "" },
+  { id: 9, image: "" },
+  { id: 10, image: "" },
 ];
 
-//rework this not mutate an array
+// research further to check if it is necessary to rework this so it doesn't mutate array
 export function shuffle(array) {
   let currentIndex = array.length;
 
@@ -24,4 +26,19 @@ export function shuffle(array) {
       array[currentIndex],
     ];
   }
+}
+
+function Image({ pokemon }) {
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((response) => response.json())
+      .then((json) => setInfo(json))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const imageUrl = info.sprites.front_default;
+
+  return imageUrl;
 }
